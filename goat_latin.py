bc8dce8a5568af2e59133464c2c75538756ed8e9
casuallysentient <reilly.ford@gmail.com>
2021-03-13 13:57:28 -0500

    forgot to do default value for step

--- goat_latin.py
+++ goat_latin.py
@@ -1,5 +1,35 @@
 def translate_sentence_to_goat_latin(original_sentence):
-    pass
+    original_word = ""
+    new_sentence = ""
+    word_number = 1
+    vowels = "aeiou"
+    for x in range(len(original_sentence)):
+        if original_sentence[x] == " ":
+            if original_word[0] in vowels:
+                new_word = original_word + "ma"
+            else:
+                new_word = original_word + original_word[0] + "ma"
+            for y in range(word_number):
+                new_word = new_word + "a"
+            new_word = new_word[1:]
+            new_sentence += new_word
+            original_word = ""
+            new_word = ""
+            new_sentence += " "
+            word_number += 1
+            continue
+        if x == len(original_sentence) - 1:
+            original_word += original_sentence[x]
+            if original_word[0] in vowels:
+                new_word = original_word + "ma"
+            else:
+                new_word = original_word + original_word[0] + "ma"
+            for y in range(word_number):
+                new_word = new_word + "a"
+            new_word = new_word[1:]
+            new_sentence += new_word
+            return new_sentence
+        original_word += original_sentence[x]
 
 
 def main():
